c072f240dd57e6bf7ad1f19d0c8d8d98cb1c90f4
Mara <Mara@MARA-LLORET>
2024-10-17 08:41:38 -0300

    arme de nuevo tomar foto con youtube ahora guarda la imagen en la ruta pero no imprime aun

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,22 +37,23 @@
             this.label1Contar = new System.Windows.Forms.Label();
             this.timeContar = new System.Windows.Forms.Timer(this.components);
             this.pictureBoxCapturada = new System.Windows.Forms.PictureBox();
+            this.btnGrabar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVideo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCapturada)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBoxVideo
             // 
-            this.pictureBoxVideo.Location = new System.Drawing.Point(25, 60);
+            this.pictureBoxVideo.Location = new System.Drawing.Point(25, 71);
             this.pictureBoxVideo.Name = "pictureBoxVideo";
-            this.pictureBoxVideo.Size = new System.Drawing.Size(481, 450);
+            this.pictureBoxVideo.Size = new System.Drawing.Size(605, 555);
             this.pictureBoxVideo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxVideo.TabIndex = 1;
             this.pictureBoxVideo.TabStop = false;
             // 
             // btnImprimir
             // 
-            this.btnImprimir.Location = new System.Drawing.Point(277, 633);
+            this.btnImprimir.Location = new System.Drawing.Point(272, 683);
             this.btnImprimir.Name = "btnImprimir";
             this.btnImprimir.Size = new System.Drawing.Size(75, 23);
             this.btnImprimir.TabIndex = 2;
@@ -64,9 +65,8 @@
             this.comboBoxCamaras.FormattingEnabled = true;
             this.comboBoxCamaras.Location = new System.Drawing.Point(25, 33);
             this.comboBoxCamaras.Name = "comboBoxCamaras";
-            this.comboBoxCamaras.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxCamaras.Size = new System.Drawing.Size(207, 21);
             this.comboBoxCamaras.TabIndex = 3;
-            this.comboBoxCamaras.SelectedIndexChanged += new System.EventHandler(this.comboBoxCamaras_SelectedIndexChanged);
             // 
             // btnDetener
             // 
@@ -76,11 +76,10 @@
             this.btnDetener.TabIndex = 4;
             this.btnDetener.Text = "Detener";
             this.btnDetener.UseVisualStyleBackColor = true;
-            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
             // 
             // btnTomarFoto
             // 
-            this.btnTomarFoto.Location = new System.Drawing.Point(134, 516);
+            this.btnTomarFoto.Location = new System.Drawing.Point(796, 31);
             this.btnTomarFoto.Name = "btnTomarFoto";
             this.btnTomarFoto.Size = new System.Drawing.Size(75, 23);
             this.btnTomarFoto.TabIndex = 5;
@@ -94,7 +93,7 @@
             this.label1Contar.BackColor = System.Drawing.SystemColors.Menu;
             this.label1Contar.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1Contar.ForeColor = System.Drawing.Color.Red;
-            this.label1Contar.Location = new System.Drawing.Point(587, 185);
+            this.label1Contar.Location = new System.Drawing.Point(664, 183);
             this.label1Contar.Name = "label1Contar";
             this.label1Contar.Size = new System.Drawing.Size(0, 108);
             this.label1Contar.TabIndex = 6;
@@ -102,22 +101,32 @@
             // timeContar
             // 
             this.timeContar.Interval = 1000;
-            this.timeContar.Tick += new System.EventHandler(this.timeContar_Tick);
             // 
             // pictureBoxCapturada
             // 
-            this.pictureBoxCapturada.Location = new System.Drawing.Point(746, 51);
+            this.pictureBoxCapturada.Location = new System.Drawing.Point(796, 62);
             this.pictureBoxCapturada.Name = "pictureBoxCapturada";
-            this.pictureBoxCapturada.Size = new System.Drawing.Size(508, 459);
+            this.pictureBoxCapturada.Size = new System.Drawing.Size(543, 564);
             this.pictureBoxCapturada.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxCapturada.TabIndex = 7;
             this.pictureBoxCapturada.TabStop = false;
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.Location = new System.Drawing.Point(272, 33);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(88, 21);
+            this.btnGrabar.TabIndex = 8;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            this.btnGrabar.Click += new System.EventHandler(this.btnGrabar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1447, 730);
+            this.Controls.Add(this.btnGrabar);
             this.Controls.Add(this.pictureBoxCapturada);
             this.Controls.Add(this.label1Contar);
             this.Controls.Add(this.btnTomarFoto);
@@ -127,6 +136,7 @@
             this.Controls.Add(this.pictureBoxVideo);
             this.Name = "Form1";
             this.Text = "Cabina de Fotos";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVideo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCapturada)).EndInit();
@@ -144,6 +154,7 @@
         private System.Windows.Forms.Label label1Contar;
         private System.Windows.Forms.Timer timeContar;
         private System.Windows.Forms.PictureBox pictureBoxCapturada;
+        private System.Windows.Forms.Button btnGrabar;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -11,174 +11,81 @@ using System.Windows.Forms;
 
 using AForge.Video;
 using System.Diagnostics.Tracing;
+using System.Drawing.Imaging;
 
 namespace cabinaFotos
 {
     public partial class Form1 : Form
     {
-        private Camara camaraManager;
-        private int countdown = 3;
 
+        public string Path = @"C:\Users\Mara\Pictures\CAMARA";
+        private bool hayDispositivos;
+        private FilterInfoCollection MisDispositivos;
+        private VideoCaptureDevice MiWebCam;
         public Form1()
         {
             InitializeComponent();
-            InicializarCamaraManager();
+         
+  
         }
 
-        /// <summary>
-        /// Inicializa la instancia de CamaraManager y configura eventos.
-        /// </summary>
-        private void InicializarCamaraManager()
-        {
-            camaraManager = new Camara();
-            camaraManager.OnNewFrame += CamaraManager_OnNewFrame;
-            camaraManager.OnError += CamaraManager_OnError;
-
-            // Llenar el ComboBox con las cámaras disponibles
-            string[] camaras = camaraManager.ObtenerCamarasDisponibles();
-            if (camaras.Length > 0)
+        public void CargaDispositivos() {
+            MisDispositivos = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (MisDispositivos.Count > 0)
             {
-                comboBoxCamaras.Items.AddRange(camaras);
-                comboBoxCamaras.SelectedIndex = 0;
+                hayDispositivos = true;
+                for(int i = 0; i < MisDispositivos.Count; i++)
+                    comboBoxCamaras.Items.Add(MisDispositivos[i].Name.ToString());
+                comboBoxCamaras.Text = MisDispositivos[0].Name.ToString();
             }
-            else
-            {
-                MessageBox.Show("No se encontraron cámaras.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
-        /// <summary>
-        /// Maneja el evento de nuevo cuadro de video.
-        /// </summary>
-        private void CamaraManager_OnNewFrame(object sender, NewFrameEventArgs eventArgs)
-        {
-           
-
-            // Asegurarse de que la actualización de la UI se realice en el hilo principal
-            if (pictureBoxVideo.InvokeRequired)
-            {
-                pictureBoxVideo.Invoke(new Action(() =>
-                {
-                    pictureBoxVideo.Image?.Dispose();
-                    pictureBoxVideo.Image = (Bitmap)eventArgs.Frame.Clone();
-                }));
-            }
-            else
-            {
-                pictureBoxVideo.Image?.Dispose();
-                pictureBoxVideo.Image = (Bitmap)eventArgs.Frame.Clone();
-            }
-        }
-
-        /// <summary>
-        /// Maneja el evento de error de la cámara.
-        /// </summary>
-        private void CamaraManager_OnError(object sender, EventArgs e)
-        {
-            MessageBox.Show("Ocurrió un error con la cámara.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-        }
-
-        /// <summary>
-        /// Evento al cambiar la selección de la cámara en el ComboBox.
-        /// </summary>
-        private void comboBoxCamaras_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            try
-            {
-                camaraManager.IniciarCamara(comboBoxCamaras.SelectedIndex);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error al iniciar la cámara: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
-       
-
-        /// <summary>
-        /// Evento al hacer clic en el botón Detener Cámara.
-        /// </summary>
-        private void btnDetener_Click(object sender, EventArgs e)
-        {
-            camaraManager.DetenerCamara();
-            pictureBoxVideo.Image = null;
-        }
-
-        /// <summary>
-        /// Asegura que la cámara se detenga al cerrar el formulario.
-        /// </summary>
-        protected override void OnFormClosing(FormClosingEventArgs e)
-        {
-            camaraManager.DetenerCamara();
-            base.OnFormClosing(e);
-        }
-
-        private void btnTomarFoto_Click(object sender, EventArgs e)
-        {
-            btnTomarFoto.Enabled = false; // Deshabilitar el botón para evitar múltiples clics
-            countdown = 3; // Inicializar el conteo
-            label1Contar.Text = countdown.ToString(); // Mostrar el conteo inicial
-            timeContar.Start(); // Iniciar el Timer
+            else  
+                hayDispositivos=false;
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            CargaDispositivos();
         }
 
-        private void timeContar_Tick(object sender, EventArgs e)
+        private void CerrarWebCam() {
+
+            if (MiWebCam != null && MiWebCam.IsRunning) { 
+            
+                MiWebCam.SignalToStop();
+                MiWebCam = null;
+            
+            }
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
         {
-            countdown--;
-
-            if (countdown > 0)
-            {
-                label1Contar.Text = countdown.ToString(); // Actualizar la etiqueta con el conteo
-            }
-            else
-            {
-                timeContar.Stop(); // Detener el Timer
-                label1Contar.Text = ""; // Limpiar la etiqueta
-                btnTomarFoto.Enabled = true; // Reactivar el botón
-
-                // Capturar la imagen
-                CapturarImagen();
-            }
+            CerrarWebCam();
+            int i = comboBoxCamaras.SelectedIndex;
+            string NombreVideo = MisDispositivos[i].MonikerString;
+            MiWebCam = new VideoCaptureDevice(NombreVideo);
+            MiWebCam.NewFrame += new NewFrameEventHandler(Capturando);
+            MiWebCam.Start();
 
         }
 
-        private void CapturarImagen()
+        private void Capturando(object sender, NewFrameEventArgs eventArgs) { 
+        Bitmap Imagen = (Bitmap)eventArgs.Frame.Clone();
+            pictureBoxVideo.Image = Imagen;
+        
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
-            if (pictureBoxVideo.Image != null)
-            {
-                // Clonar la imagen actual
-                Bitmap captura = (Bitmap)pictureBoxVideo.Image.Clone();
-
-                // Mostrar la imagen capturada en pictureBoxCapturada
-                pictureBoxCapturada.Image = captura;
-
-                // Opcional: Guardar la imagen en disco
-                string rutaGuardado = $"captura_{DateTime.Now:yyyyMMdd_HHmmss}.jpg";
-                try
-                {
-                    captura.Save(rutaGuardado, System.Drawing.Imaging.ImageFormat.Jpeg);
-                    MessageBox.Show($"Imagen capturada y guardada como {rutaGuardado}.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"Error al guardar la imagen: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-                finally
-                {
-                    captura.Dispose(); // Liberar recursos
-                }
-            }
-            else
-            {
-                MessageBox.Show("No hay imagen para capturar.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
+            CerrarWebCam();
         }
 
-
-
+        private void btnTomarFoto_Click(object sender, EventArgs e)
+        {
+            if (MiWebCam != null && MiWebCam.IsRunning) { 
+            pictureBoxCapturada.Image = pictureBoxVideo.Image;
+                pictureBoxCapturada.Image.Save(Path + "\\hdeleon.jpg", ImageFormat.Jpeg);
+            }
+        }
     }
 }
